595857a5c59d6286e87feb9db98d3c450d1266b1
StKob <203904@edu.p.lodz.pl>
2022-07-07 14:25:40 +0200

    Added javadoc comments for 1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,30 +1,61 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The type Default counting out rhymer.
+ */
 public class DefaultCountingOutRhymer {
 
 	private int[] numbers = new int[12];
 
+	/**
+	 * The Total.
+	 */
 	public int total = -1;
 
+	/**
+	 * Count in.
+	 *
+	 * @param in the in
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * Call check boolean.
+	 *
+	 * @return the boolean
+	 */
 	public boolean callCheck() {
 		return total == -1;
 	}
-		
+
+	/**
+	 * Is full boolean.
+	 *
+	 * @return the boolean
+	 */
 	public boolean isFull() {
 		return total == 11;
 	}
-		
+
+	/**
+	 * Peekaboo int.
+	 *
+	 * @return the int
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return -1;
 		return numbers[total];
 	}
-			
+
+	/**
+	 * Count out int.
+	 *
+	 * @return the int
+	 */
 	public int countOut() {
 		if (callCheck())
 			return -1;
