510ecd18e4d8898145ae25cbe53056036bfe9384
StKob <203904@edu.p.lodz.pl>
2022-09-19 14:05:58 +0200

    Intendation and reformatting

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,71 +5,71 @@ package edu.kis.vh.nursery;
  */
 public class DefaultCountingOutRhymer {
 
-    public static final int EMPTY = -1;
-    public static final int MAX_SIZE = 12;
-    public static final int FULL = MAX_SIZE - 1;
-    private final int[] numbers = new int[MAX_SIZE];
-    private int total = EMPTY;
+	public static final int EMPTY = -1;
+	public static final int MAX_SIZE = 12;
+	public static final int FULL = MAX_SIZE - 1;
+	private final int[] numbers = new int[MAX_SIZE];
+	private int total = EMPTY;
 
 
-    /**
-     * Get total int.
-     *
-     * @return the int
-     */
-    public int getTotal() {
-        return total;
-    }
+	/**
+	 * Get total int.
+	 *
+	 * @return the int
+	 */
+	public int getTotal() {
+		return total;
+	}
 
 
-    /**
-     * Count in.
-     *
-     * @param in the in
-     */
-    public void countIn(int in) {
-        if (!isFull())
-            numbers[++total] = in;
-    }
+	/**
+	 * Count in.
+	 *
+	 * @param in the in
+	 */
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
 
-    /**
-     * Call check boolean.
-     *
-     * @return the boolean
-     */
-    public boolean callCheck() {
-        return total == EMPTY;
-    }
+	/**
+	 * Call check boolean.
+	 *
+	 * @return the boolean
+	 */
+	public boolean callCheck() {
+		return total == EMPTY;
+	}
 
-    /**
-     * Is full boolean.
-     *
-     * @return the boolean
-     */
-    public boolean isFull() {
-        return total == FULL;
-    }
+	/**
+	 * Is full boolean.
+	 *
+	 * @return the boolean
+	 */
+	public boolean isFull() {
+		return total == FULL;
+	}
 
-    /**
-     * Peekaboo int.
-     *
-     * @return the int
-     */
-    protected int peekaboo() {
-        if (callCheck())
-            return EMPTY;
-        return numbers[total];
-    }
+	/**
+	 * Peekaboo int.
+	 *
+	 * @return the int
+	 */
+	protected int peekaboo() {
+		if (callCheck())
+			return EMPTY;
+		return numbers[total];
+	}
 
-    /**
-     * Count out int.
-     *
-     * @return the int
-     */
-    public int countOut() {
-        if (callCheck())
-            return EMPTY;
-        return numbers[total--];
-    }
+	/**
+	 * Count out int.
+	 *
+	 * @return the int
+	 */
+	public int countOut() {
+		if (callCheck())
+			return EMPTY;
+		return numbers[total--];
+	}
 
 }
